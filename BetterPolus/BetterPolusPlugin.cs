96634f968c3ed3f28b122bd7537ac0d6b053f755
amsyarasyiq <amsyarasyiq@gmail.com>
2021-05-21 16:23:53 +0800

    update to support v2021.5.10
    and removed Reactor dependency because it's useless for this mod

--- BetterPolus/BetterPolusPlugin.cs
+++ BetterPolus/BetterPolusPlugin.cs
@@ -2,36 +2,25 @@
 using BepInEx.IL2CPP;
 using BepInEx.Logging;
 using HarmonyLib;
-using Reactor;
-using Reactor.Patches;
 
-namespace BetterPolus
-{
+namespace BetterPolus{
     [BepInPlugin(Id)]
     [BepInProcess("Among Us.exe")]
-    [BepInDependency(ReactorPlugin.Id)]
-    public class BetterPolusPlugin : BasePlugin
-    {
+    public class BetterPolusPlugin : BasePlugin{
         public const string Id = "ch.brybry.betterpolus";
-
         public Harmony Harmony { get; } = new Harmony(Id);
         public static ManualLogSource log;
-
-
-        public override void Load()
-        {
+        public override void Load(){
             log = Log;
-            
-            log.LogMessage("BetterPolus Mod loaded");
-
-            ReactorVersionShower.TextUpdated += (text) =>
-            {
-                int index = text.Text.LastIndexOf('\n');
-                text.Text = text.Text.Insert(index == -1 ? text.Text.Length - 1 : index, 
-                    "\nLoaded [5E4CA6FF]BetterPolus v1.1.2-R []by Brybry");
-            };
-            
+            log.LogMessage("Succesfully loaded BetterPolus");
             Harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(VersionShower), nameof(VersionShower.Start))]
+    public static class VersionShowerUpdate {
+        public static void Postfix(VersionShower __instance){
+            __instance.text.text += " - <color=#5E4CA6FF>BetterPolus v1.2.0-R</color>";
         }
     }
 }
